4c2e5c2584fb64ec6b4305ba264875fe35fe6291
Silvio Sepulveda <silvio.sepulveda@kivra.com>
2021-10-26 16:10:49 +0200

    Added partial support for object metadata

--- src/enenra_server.erl
+++ src/enenra_server.erl
@@ -260,11 +260,7 @@ upload_object(Object, RequestBody, Token) ->
         {<<"X-Upload-Content-Type">>, Object#object.contentType},
         {<<"X-Upload-Content-Length">>, Object#object.size}
     ]),
-    ReqBody = binary_to_list(jiffy:encode({[
-        {<<"name">>, Object#object.name},
-        % include the md5 so GCP can verify the upload was successful
-        {<<"md5Hash">>, Object#object.md5Hash}
-    ]})),
+    ReqBody = binary_to_list(jiffy:encode(upload_object_body(Object))),
     {ok, Status, Headers, Client} = hackney:request(post, Url, ReqHeaders, ReqBody),
     case Status of
         200 ->
@@ -274,6 +270,18 @@ upload_object(Object, RequestBody, Token) ->
             do_upload(UploadUrl, Object, RequestBody, Token);
         _ -> decode_response(Status, Headers, Client)
     end.
+
+% @doc
+%
+% Creates the body for an object upload based on the object record
+%
+-spec upload_object_body(object()) -> tuple().
+upload_object_body(#object{name = Name, md5Hash = Hash, metadata = Metadata}) ->
+    {add_metadata(Metadata, [
+        {<<"name">>, Name},
+        % include the md5 so GCP can verify the upload was successful
+        {<<"md5Hash">>, Hash}
+    ])}.
 
 % @doc
 %
@@ -426,7 +434,8 @@ make_object(PropList) ->
         updated=proplists:get_value(<<"updated">>, PropList),
         storageClass=proplists:get_value(<<"storageClass">>, PropList),
         size=proplists:get_value(<<"size">>, PropList),
-        md5Hash=proplists:get_value(<<"md5Hash">>, PropList)
+        md5Hash=proplists:get_value(<<"md5Hash">>, PropList),
+        metadata=add_metadata(PropList, [])
     }.
 
 % @doc
@@ -551,3 +560,30 @@ compute_signature(PemKeyBin, Msg) ->
     [PemKeyData] = public_key:pem_decode(PemKeyBin),
     PemKey = public_key:pem_entry_decode(PemKeyData),
     base64:encode(public_key:sign(Msg, sha256, PemKey)).
+
+% @doc
+%
+% Adds metadata values if any. Unrecognized values are ignored.
+%
+% Names taken from:
+%    https://cloud.google.com/storage/docs/json_api/v1/objects#resource-representations
+%
+-spec add_metadata(list(), list()) -> list().
+add_metadata([], List) ->
+    List;
+add_metadata([{<<"cacheControl">>, _V} = M | T], List) ->
+    add_metadata(T, [M | List]);
+add_metadata([{<<"contentDisposition">>, _V} = M | T], List) ->
+    add_metadata(T, [M | List]);
+add_metadata([{<<"contentEncoding">>, _V} = M | T], List) ->
+    add_metadata(T, [M | List]);
+add_metadata([{<<"contentLanguage">>, _V} = M | T], List) ->
+    add_metadata(T, [M | List]);
+add_metadata([{<<"contentType">>, _V} = M | T], List) ->
+    add_metadata(T, [M | List]);
+add_metadata([{<<"customTime">>, _V} = M | T], List) ->
+    add_metadata(T, [M | List]);
+add_metadata([{<<"metadata">>, _V} = M | T], List) ->
+    add_metadata(T, [M | List]);
+add_metadata([_H | T], List) ->
+    add_metadata(T, List).
